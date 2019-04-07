e0d52b81e751b281eb18c9acc5e9c1ceb76bdac5
Josh <squashroe@gmail.com>
2019-04-07 13:06:44 +0100

    background and settings done, and a png image of a maze background just in case

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,32 @@
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    public void start(Stage primaryStage) throws Exception {
+    private Pane playfieldLayer;
+    private Pane scoreLayer;
+    private Scene scene;
+
+    public void start(Stage primaryStage) {
+
+        Group root = new Group();
+
+        //create the layers
+        playfieldLayer = new Pane();
+        scoreLayer = new Pane();
+
+        root.getChildren().add(playfieldLayer);
+        root.getChildren().add(scoreLayer);
+
+        scene = new Scene(root, Settings.SCENE_WIDTH, Settings.SCENE_HEIGHT);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+
 
     }
 }
